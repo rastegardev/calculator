e884baced7144ba569ccbe8edff084bdf66b4a5d
reza rastegar <85369490+rezarastegar2003@users.noreply.github.com>
2022-07-16 18:58:05 +0200

    Add files via upload

---
+++ js/save.js
@@ -0,0 +1,34 @@
+const displayNumber = document.querySelector(".display");
+const keypadBtn = document.querySelectorAll(".show-display");
+
+function showDisplay(event) {
+  const x = event.target.innerText;
+  if (displayNumber.innerText == 0) {
+    return (displayNumber.innerText = x);
+  }
+  return (displayNumber.innerText += x);
+}
+
+function calculate() {
+  displayNumber.innerText = eval(result).toFixed(4);
+}
+
+function allClear() {
+  displayNumber.innerText = 0;
+}
+
+function clearLast() {
+  let userSelect = displayNumber.innerText;
+  if (userSelect.length == 1) {
+    displayNumber.innerText = 0;
+  } else {
+    displayNumber.innerText = userSelect.substring(0, userSelect.length - 1);
+  }
+}
+
+document.querySelector(".calculate").addEventListener("click", calculate);
+document.querySelector(".all-clear").addEventListener("click", allClear);
+document.querySelector(".clear-last").addEventListener("click", clearLast);
+keypadBtn.forEach((item) => {
+  item.addEventListener("click", showDisplay);
+});
